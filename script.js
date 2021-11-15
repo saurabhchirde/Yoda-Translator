c002b5ac81054abaf8c480dccdd03bbab28e764f
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-15 15:59:28 +0530

    update

--- script.js
+++ script.js
@@ -3,29 +3,30 @@ const btnClear = document.querySelector("#btn-clear");
 const btnTranslate = document.querySelector("#btn-translate");
 const divOut = document.querySelector("#output");
 
-serverURL = "https://api.funtranslations.com/translate/yoda.json";
-
-btnClear.addEventListener("click", function () {
-  txtInput.value = "";
-  divOut.innerText = "";
-});
+const serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
 function makeURLcomplete(text) {
   return serverURL + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
-  console.log("Error occured " + error);
+  console.log("Error occured ", error);
   alert("Server is not rechable :( try again later");
 }
+
 btnTranslate.addEventListener("click", function () {
   const txt = txtInput.value;
 
   fetch(makeURLcomplete(txt))
-    .then((takingRes) => takingRes.json())
-    .then((innerValue) => {
-      const result = innerValue.contents.translated;
+    .then((response) => response.json())
+    .then((json) => {
+      var result = json.contents.translated;
       divOut.innerText = result;
     })
     .catch(errorHandler);
+});
+
+btnClear.addEventListener("click", function () {
+  txtInput.value = "";
+  divOut.innerText = "";
 });
